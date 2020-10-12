05fa0af2b2ed778d007e8162ce5abb0a1337c147
Shirwa Warsame <shirwarsame@gmail.com>
2020-10-12 00:07:02 -0400

    Fixed the pointers in Part 4

--- COMP345 - PROJECT/assignment1/OrdersDriver.cpp
+++ COMP345 - PROJECT/assignment1/OrdersDriver.cpp
@@ -1,71 +1,53 @@
 #include "Orders.h"
-int main()
-{
-	OrdersList orderList;
 
-	//Create pointers of subclasses
-	Deploy *deploy1 = new Deploy();
-	Advance *advance1 = new Advance();
-	Blockade *blockade1 = new Blockade();
-	Airlift *airlift1 = new Airlift();
-	Negotiate *negotiate1 = new Negotiate();
-	Bomb *bomb1 = new Bomb();
+int main() {
+	Order* order = new Order();
 
-	Bomb *bomb2 = new Bomb();
+	OrdersList* orderList = new OrdersList();
 
-	std::vector<Order> newList;
+	Order* deploy1 = new Deploy();
+	Advance* advance1 = new Advance();
+	Blockade* blockade1 = new Blockade();
+	Airlift* airlift1 = new Airlift();
+	Negotiate* negotiate1 = new Negotiate();
+	Bomb* bomb1 = new Bomb();
 
-	//Push objects to vector of objects named newList
-	newList.push_back(*deploy1);
-	newList.push_back(*advance1);
-	newList.push_back(*blockade1);
-	newList.push_back(*bomb2);
-	newList.push_back(*airlift1);
-	newList.push_back(*negotiate1);
-	newList.push_back(*bomb1);
+	Bomb* bomb2 = new Bomb();
 
-	std::cout << *deploy1 << std::endl;
-	std::cout << "\n"
-			  << std::endl;
+	//std::cout << *deploy1 << endl;
+	std::vector<Order*> newList;
 
-	//Execute bomb2 order and print result
+	newList.push_back(deploy1);
+	newList.push_back(advance1);
+	newList.push_back(blockade1);
+	newList.push_back(bomb2);
+	newList.push_back(airlift1);
+	newList.push_back(negotiate1);
+	newList.push_back(bomb1);
+
+
 	bomb2->execute();
+	std::cout << "Output the change in the ostream after executing order:" << std::endl;
 	std::cout << *bomb2 << std::endl;
+	
+	orderList->setList(newList);
 
-	//Assign listOfOrders to newlist
-	orderList.setList(newList);
-
-	std::cout << "\n"
-			  << std::endl;
-	//Delete bomb2 order and print result
-	orderList.delete_order(3);
-	for (unsigned int i = 0; i < orderList.getList().size(); i++)
-	{
-		std::cout << orderList.getList()[i] << std::endl;
-	}
-	std::cout << "\n"
-			  << std::endl;
-
-	//Move the order in position 0 to position 5 and print result
-	orderList.move(0, 5);
-	for (unsigned int i = 0; i < orderList.getList().size(); i++)
-	{
-		std::cout << orderList.getList()[i] << std::endl;
+	std::cout << std::endl;
+	std::cout << "Print out the list of orders:" << std::endl;
+	for (unsigned int i = 0; i < (orderList->getList().size()); i++) {
+		std::cout<< "Order "<<i<<": " << *(orderList->getList()[i]) << std::endl;
 	}
 
-	//Remove memory leaks
-	delete (deploy1);
-	deploy1 = NULL;
-	delete (advance1);
-	advance1 = NULL;
-	delete (blockade1);
-	blockade1 = NULL;
-	delete (airlift1);
-	airlift1 = NULL;
-	delete (negotiate1);
-	negotiate1 = NULL;
-	delete (bomb1);
-	bomb1 = NULL;
-	delete (bomb2);
-	bomb2 = NULL;
+	
+	orderList->delete_order(3);
+	orderList->move(0, 5);
+	std::cout << std::endl;
+	std::cout << "AFTER deleting the order at position 3 and MOVING 0 tO 5:" << std::endl;
+	for (unsigned int i = 0; i < (orderList->getList().size()); i++) {
+
+		std::cout << "Order " << i << ": " << *(orderList->getList()[i]) << std::endl;
+	}
+
+	delete(orderList);
+
 }
