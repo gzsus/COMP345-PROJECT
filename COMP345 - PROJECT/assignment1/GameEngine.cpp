69781ef8cf21902788413272b430bf62160d0b50
Samuel Renaud <s_renaud@live.concordia.ca>
2020-11-03 10:59:02 -0500

    deleted the GameEngineDriver as it says in the instructions to implement a main in the GameEngine.cpp

--- COMP345 - PROJECT/assignment1/GameEngine.cpp
+++ COMP345 - PROJECT/assignment1/GameEngine.cpp
@@ -6,7 +6,7 @@
 
 
 //default constructor
-GameEngine::GameEngine()
+GameEngine::GameEngine() :  game_map()
 {}
 
 GameEngine::~GameEngine()
@@ -18,16 +18,39 @@ GameEngine::~GameEngine()
 //used to pick a map
 std::string GameEngine::getmap()
 {
-
-	std::cout << "Let's Pick a map!\n";
+	std::vector<std::string> maps;
+	std::cout << "Let's Pick a map!\n\n";
 	try {
-		
 		for (auto& p : std::filesystem::directory_iterator("map_dir"))
-			std::cout << p.path() << '\n';
+			maps.push_back(p.path().string());
 	}
-
 	catch (int e) {std::cout << "I/O error";}
-	
+
+	//need to remove the first 8 characters to show only map and not path when asking for choice
+	for (int i = 0; i < maps.size(); i++)
+	{
+		std::cout << "Map #"<<i<<" -> "<<maps[i].substr(8)<<"\n";
+	}
+	std::cout << "Please Enter the number of the map which you would like to select: \n";
+	int map_number;
+	std::cin>> map_number;
+
+
+
 
 	return std::string();
+}
+
+
+
+
+int main()
+{
+	std::cout << "-----Welcom To Risk!-----\n\n";
+	GameEngine game;
+	game.getmap();
+
+	game.~GameEngine();
+
+	return 0;
 }
